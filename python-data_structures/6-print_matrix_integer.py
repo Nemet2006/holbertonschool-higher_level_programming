d7d9c8bd87ee224534913e942cd59a309043b599
Nemet2006 <nemtzrbiyev2006@gmail.com>
2024-09-18 21:32:08 +0400

    commit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, value in enumerate(row):
-            print("{:d}".format(value),end="")
+            print("{:d}".format(value), end="")
             if i != (len(row) - 1):
                 print(" ", end="")
         print("")
